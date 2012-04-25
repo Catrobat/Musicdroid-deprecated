201b4c031d5c1ab5dd73eeaaca731ded5a2b7243
Valentin <valentin.kassarnig@student.tugraz.at>
2012-04-25 16:30:47 +0200

    just a test!

--- src/at/tugraz/ist/musicdroid/MusicdroidActivity.java
+++ src/at/tugraz/ist/musicdroid/MusicdroidActivity.java
@@ -9,5 +9,6 @@ public class MusicdroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // first change!!!
     }
 }
