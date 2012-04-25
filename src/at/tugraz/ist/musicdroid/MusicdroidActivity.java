e6191ef76167bba3a83506baea80fe009904a344
Rudolf Wagner <rudolf.wagner@student.tugraz.at>
2012-04-25 15:53:53 +0200

    Clean Project

---
+++ src/at/tugraz/ist/musicdroid/MusicdroidActivity.java
@@ -0,0 +1,13 @@
+package at.tugraz.ist.musicdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MusicdroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
