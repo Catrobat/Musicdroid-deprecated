f8d4d1ebe85a3704e1b9ce16eba94462dbeedb35
Rudolf Wagner <rudolf.wagner@student.tugraz.at>
2012-04-30 13:09:08 +0200

    test eclipse

--- src/at/tugraz/ist/musicdroid/MusicdroidActivity.java
+++ src/at/tugraz/ist/musicdroid/MusicdroidActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 public class MusicdroidActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
-    
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
