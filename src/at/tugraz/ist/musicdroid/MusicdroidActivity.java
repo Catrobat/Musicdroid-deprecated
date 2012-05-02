e0194a6a8baa5b25fd5876873f81078804a242ea
Rudolf Wagner <rudolf.wagner@student.tugraz.at>
2012-05-02 14:10:10 +0200

    new branch start_app

--- src/at/tugraz/ist/musicdroid/MusicdroidActivity.java
+++ src/at/tugraz/ist/musicdroid/MusicdroidActivity.java
@@ -11,7 +11,7 @@ public class MusicdroidActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        // Seventh change!!! (Bianca ;-) )
+        // Seventh change!!! (Bianca ;-)
         
     }
 }
