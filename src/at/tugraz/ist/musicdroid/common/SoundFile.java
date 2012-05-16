71caa29f65f1b7e13fcd72c84fd31c10d5355064
Rene Obendrauf <obendrauf@student.tugraz.at>
2012-05-16 13:22:18 +0200

    applied test

--- src/at/tugraz/ist/musicdroid/common/SoundFile.java
+++ src/at/tugraz/ist/musicdroid/common/SoundFile.java
@@ -17,7 +17,7 @@ public class SoundFile {
 
 	private Context context;
 	private static SoundFile instance = null;
-	final int REQUEST_CODE = 0;
+	public static final int REQUEST_CODE = 0;
 	ArrayList<String> file_list_ = new ArrayList<String>();
 
 	private SoundFile() {
