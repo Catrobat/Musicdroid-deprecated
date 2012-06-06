30ca0bf3c1ef8a11e4607cd65b96a233fa701b4e
Andreas Fruehwirt <andreas.fruehwirt@gmail.com>
2012-06-06 09:14:13 +0200

    Piano Interface

---
+++ src/at/tugraz/ist/musicdroid/PianoActivity.java
@@ -0,0 +1,14 @@
+package at.tugraz.ist.musicdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PianoActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.piano);
+        
+	}
+
+}
