4b25a4981506c553381e2f1590d4afb798247a12
daniel <daniel@daniel-VPCF12M1E>
2012-06-13 15:20:32 +0200

    newest incl. scrolling

--- src/at/tugraz/ist/musicdroid/Tone.java
+++ src/at/tugraz/ist/musicdroid/Tone.java
@@ -1,5 +1,6 @@
 package at.tugraz.ist.musicdroid;
 
+import java.util.ArrayList;
 import java.util.Dictionary;
 
 import android.content.Context;
@@ -16,20 +17,23 @@ public class Tone extends View {
 	private Resources res = getResources();
 	private int r;
 	private int x;
-	private int y;
-	private int midiVal;
+	private int c_2_y;
+	private ArrayList<Integer> y_list = new ArrayList();
+	private ArrayList<Integer> midiVal = new ArrayList();
 	private int help_value;
-	private boolean cross = false;
+	private ArrayList<Boolean> cross= new ArrayList();
 	private Paint paint;
 	private int y_line;
 
-	public Tone(Context context, int midiVal, int x, int y, Paint paint) {
+	public Tone(Context context, ArrayList<Integer> midiVal, int x, int y,
+			Paint paint) {
 		// bekomme y = oberste Notenzeile
+
 		super(context);
 		r = res.getInteger(R.integer.radius);
 		this.midiVal = midiVal;
 		this.x = x;
-		this.y = y + 4 * r;
+		this.c_2_y = y + 4 * r;
 		this.y_line = y;
 		calculateCoordinates();
 		this.paint = paint;
@@ -38,53 +42,64 @@ public class Tone extends View {
 
 	private void calculateCoordinates() {
 		// g2=43 x+r, y-r
-		int octave = midiVal / 12 - 1;
-		int value = midiVal % 12; // 0=C
+		for (int i = 0; i < midiVal.size(); i++) {
+			int octave = midiVal.get(i) / 12 - 4;
+			int value = midiVal.get(i) % 12; // 0=C
 
-		int distance[] = { 6, 6, 5, 5, 4, 3, 3, 2, 2, 1, 1, 0 };
+			int distance[] = { 6, 6, 5, 5, 4, 3, 3, 2, 2, 1, 1, 0 };
 
-		help_value = distance[value];
+			help_value = distance[value];
 
-		if (value == 1 || value == 3 || value == 6 || value == 8 || value == 10)
-			cross = true;
+			if (value == 1 || value == 3 || value == 6 || value == 8
+					|| value == 10)
+				cross.add(true);
+			else
+				cross.add(false);
 
-		y += help_value * r - (octave - 1) * 2 * r * 3.5;
+			y_list.add((int) (c_2_y + help_value * r - (octave - 1) * 2 * r
+					* 3.5));
+		}
 	}
 
 	@Override
 	public void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
+		for (int i = 0; i < midiVal.size(); i++) {
 
-		RectF oval = new RectF(x - r - r / 4, y - r, x + r + r / 4, y + r);
+			int y = y_list.get(i);
 
-		canvas.drawOval(oval, paint);
-		if (this.midiVal < 36)
-			canvas.drawLine(x + r + r / 4, y, x + r + r / 4, y - 3 * r, paint);// nach
-		// unten
-		// klappen
-		else {
-			canvas.drawLine(x - r - r / 4, y, x - r - r / 4, y + 3 * r, paint);
-		}
-		int helpline = y_line + 10 * r;
-		while (helpline <= y) {
-			canvas.drawLine(x - 2 * r, helpline, x + 2 * r, helpline, paint);
-			helpline += 2 * r;
-		}
-		helpline = y_line - 2 * r;
-		while (helpline >= y) {
-			canvas.drawLine(x - r * 2, helpline, x + r * 2, helpline, paint);
-			helpline -= 2 * r;
-		}
+			RectF oval = new RectF(x - r - r / 4, y - r, x + r + r / 4, y + r);
 
-		if (cross) {
-			canvas.drawLine(x - 3 * r, y - 2 * r, x - 3 * r, y + 2 * r - 1,
-					paint);
-			canvas.drawLine(x - 2 * r, y - 2 * r, x - 2 * r, y + 2 * r - 1,
-					paint);
+			canvas.drawOval(oval, paint);
+			if (this.midiVal.get(i) < 72)
+				canvas.drawLine(x + r + r / 4, y, x + r + r / 4, y - 3 * r,
+						paint);
 
-			canvas.drawLine(x - 4 * r, y, x - 3 / 2 * r, y - r, paint);
+			else {
+				canvas.drawLine(x - r - r / 4, y, x - r - r / 4, y + 3 * r,
+						paint);
+			}
+			int helpline = y_line + 10 * r;
+			while (helpline <= y) {
+				canvas.drawLine(x - 2 * r, helpline, x + 2 * r, helpline, paint);
+				helpline += 2 * r;
+			}
+			helpline = y_line - 2 * r;
+			while (helpline >= y) {
+				canvas.drawLine(x - r * 2, helpline, x + r * 2, helpline, paint);
+				helpline -= 2 * r;
+			}
 
-			canvas.drawLine(x - 4 * r, y + r, x - 3 / 2 * r, y, paint);
+			if (cross.get(i)) {
+				canvas.drawLine(x - 3 * r, y - 2 * r, x - 3 * r, y + 2 * r - 1,
+						paint);
+				canvas.drawLine(x - 2 * r, y - 2 * r, x - 2 * r, y + 2 * r - 1,
+						paint);
+
+				canvas.drawLine(x - 4 * r, y, x - 3 / 2 * r, y - r, paint);
+
+				canvas.drawLine(x - 4 * r, y + r, x - 3 / 2 * r, y, paint);
+			}
 		}
 	}
 
