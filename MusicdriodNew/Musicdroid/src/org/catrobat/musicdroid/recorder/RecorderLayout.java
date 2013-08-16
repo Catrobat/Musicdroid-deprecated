9e4c6e0872bafa26110dd29c3568533ceb9d9b4b
Matthias Schlesinger <matthias.schlesinger@student.tugraz.at>
2013-08-16 13:55:53 +0200

    MUS-34 Removed .*-imports

--- MusicdriodNew/Musicdroid/src/org/catrobat/musicdroid/recorder/RecorderLayout.java
+++ MusicdriodNew/Musicdroid/src/org/catrobat/musicdroid/recorder/RecorderLayout.java
@@ -39,7 +39,6 @@ import org.catrobat.musicdroid.R;
 import org.catrobat.musicdroid.RecorderActivity;
 import org.catrobat.musicdroid.dialog.ChangeFilenameDialog;
 import org.catrobat.musicdroid.tools.StringFormatter;
-import org.catrobat.musicdroid.tools.StringFormatter;
 
 public class RecorderLayout extends Handler implements OnClickListener,
 		OnLongClickListener {
